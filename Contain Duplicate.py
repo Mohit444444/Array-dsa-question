410857bc57ed897cf1cfa596507838dee90be4c6
Mohit yadav <y2641699@gmail.com>
2025-01-11 22:46:06 +0530

    contain duplicate

---
+++ Contain Duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False 
+
+        # time O(n)
+        # space O(1)
