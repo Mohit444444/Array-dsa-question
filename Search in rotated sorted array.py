47bcaeee46328068992cba5d5f094e9c6bb3d378
Mohit yadav <y2641699@gmail.com>
2025-01-13 11:22:37 +0530

    array dsa que

---
+++ Search in rotated sorted array.py
@@ -0,0 +1,49 @@
+# Example 1:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+
+# Input: nums = [1], target = 0
+# Output: -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        l = 0
+        r = n - 1
+
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] > nums[r]:
+                l = m + 1
+            else:
+                r = m
+
+        min_i = l
+        if min_i == 0:
+            l, r = 0, n-1
+        elif target >= nums[0] and target <= nums[min_i-1]:
+            l, r = 0, min_i
+        else:
+            l, r = min_i, n-1
+
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+
+        return -1
+
+        # time O(log n)
+        # space O(1)
