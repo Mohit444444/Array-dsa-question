4cef4b7e6b02145e452b786bc2e93db99fa09d32
Mohit yadav <y2641699@gmail.com>
2025-01-13 21:17:28 +0530

    arraydsa que

---
+++ Next permutation.py
@@ -0,0 +1,36 @@
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: [1,3,2]
+# Example 2:
+
+# Input: nums = [3,2,1]
+# Output: [1,2,3]
+# Example 3:
+
+# Input: nums = [1,1,5]
+# Output: [1,5,1]
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        if i >= 0:
+            j = len(nums) - 1
+            while j >= 0 and nums[j] <= nums[i]:
+                j -= 1
+            nums[i], nums[j] = nums[j], nums[i]
+
+        nums[i + 1:] = reversed(nums[i + 1:])
+
+        return nums
+     
+    #  time O(n)
+    #  space O(1)
+
